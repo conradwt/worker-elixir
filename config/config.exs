a3be184656a155562e438b161976dc902ed38b31
Conrad Taylor <conradwt@gmail.com>
2023-08-25 23:53:20 -0700

    Code cleanup. (#1)

--- config/config.exs
+++ config/config.exs
@@ -1,19 +1,5 @@
 import Config
 
-#
-# https://hexdocs.pm/logger/1.15.4/Logger.html
-#
-
 config :logger, :console,
   level: :error,
   format: "$time $message $metadata"
-
-# or
-
-#
-# config :logger, :default_handler,
-#   level: :error
-#
-# config :logger, :default_formatter,
-#   format: "$time $message $metadata"
-#
