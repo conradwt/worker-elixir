f04b86a00129c592f8811179261ebbc52bd6eb71
Conrad Taylor <conradwt@gmail.com>
2024-07-22 23:50:11 -0700

    Update to Erlang 27.0.1.

--- mix.exs
+++ mix.exs
@@ -23,7 +23,7 @@ defmodule Worker.MixProject do
   defp deps do
     [
       {:redix, "~> 1.3.0"},
-      {:req, "~> 0.5.2"},
+      {:req, "~> 0.5.4"},
       {:credo, "~> 1.7.7", only: [:dev, :test], runtime: false}
     ]
   end
