3de4fcf8f1dbe22d6b44c675cd602bfc7e29c153
Conrad Taylor <conradwt@gmail.com>
2025-06-23 23:32:07 -0700

    Update to Erlang 28.0.1 and Elixir 1.18.4. (#56)

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Worker.MixProject do
     [
       app: :worker,
       version: "0.1.0",
-      elixir: "~> 1.18.3",
+      elixir: "~> 1.18.4",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
