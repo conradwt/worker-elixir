005131229a6c42985e28029af90b61eab7ddd94a
Conrad Taylor <conradwt@gmail.com>
2026-01-10 18:36:09 -0800

    Update to Elixir 1.19.5.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Worker.MixProject do
     [
       app: :worker,
       version: "0.1.0",
-      elixir: "~> 1.19.4",
+      elixir: "~> 1.19.5",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -24,7 +24,7 @@ defmodule Worker.MixProject do
     [
       {:redix, "~> 1.3.0"},
       {:req, "~> 0.5.15"},
-      {:credo, "~> 1.7.14", only: [:dev, :test], runtime: false}
+      {:credo, "~> 1.7.15", only: [:dev, :test], runtime: false}
     ]
   end
 end
